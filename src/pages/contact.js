aea9a2261c9b4d615877dd862f84ed6ac03dcfb3
arjun-sudo <arjungautam5431@gmail.com>
2021-03-29 23:28:06 +0545

    contact form completed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,33 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className={"contact-page"}>
+        <article className={"contact-form"}>
+          <h3>Get in Touch</h3>
+          <form>
+            <div className={"form-group"}>
+              <input type={"text"} placeholder={"name"}
+               className={"form-control"}
+              />
+              <input type={"email"} placeholder={"email"}
+                     className={"form-control"}
+              />
+              <textarea name={"message"} rows={"5"}
+              placeholder={"message"} className={"form-control"}
+              >
+
+              </textarea>
+
+            </div>
+            <button type={"submit"} className={"submit-btn btn"}>submit here</button>
+          </form>
+        </article>
+      </section>
+
+    </Layout>
+  )
 }
 
 export default contact
