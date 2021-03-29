31bc609005125f587ac6a658cd298f252af2cdc2
arjun-sudo <arjungautam5431@gmail.com>
2021-03-29 22:05:53 +0545

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+ <footer className={"footer"}>
+  <div>
+    <SocialLinks styleClass={"footer-links"}/>
+    <h4>&copy;
+    <span>Arjun </span> {new Date().getFullYear()}, all rights reserved. Built with
+      <a href="https://www.gatsbyjs.org" target="_blank" rel="nofollow noopener noreferrer"
+       > Gatsby</a>
+
+    </h4>
+  </div>
+ </footer>
+  )
 }
 
 export default Footer
