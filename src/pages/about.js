883fee6d9ec085af5e33cc6dabf1b9f293a12a2a
arjun-sudo <arjungautam5431@gmail.com>
2021-03-29 23:12:54 +0545

    about section completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,52 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({data:{about:{nodes},},}) => {
+
+  const {info,title,stack,image}=nodes[0]
+  return (
+    <Layout>
+      <div className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid}
+                 className={"about-img"}/>
+
+          <article className={"about-text"}>
+          <Title title={title}/>
+          <p>{info}</p>
+            <div className="about-stack">
+              {stack.map((item)=>{
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </div>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    about:allStrapiAbout {
+      nodes {
+        stack {
+          id
+          title
+        }
+        title
+        info
+        image {
+          childImageSharp {
+            fluid {
+             ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About
