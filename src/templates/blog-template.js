61f1ac2ee996f468b0d9de95f7cd3b3b234aa758
arjun-sudo <arjungautam5431@gmail.com>
2021-03-29 17:45:01 +0545

    Blog post template setup complete

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -7,13 +7,13 @@ const ComponentName = () => {
   return <h2>blog post template</h2>
 }
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
 
-//     }
-//   }
-// `
+    }
+  }
+`
 
 export default ComponentName
