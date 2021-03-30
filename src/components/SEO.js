6f22758fff30acf1392d686c530bc7e4c683e9e1
arjun-sudo <arjungautam5431@gmail.com>
2021-03-30 18:15:01 +0545

    completed

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -26,7 +26,13 @@ const SEO = ({ title, description = "This is out home page" }) => {
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name={"description"} content={description || siteDesc} />
-      <meta name={"image"} content={image}/>
+      <meta name={"image"} content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creater" content={"@Laserarjun876"} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+
     </Helmet>
   )
 }
