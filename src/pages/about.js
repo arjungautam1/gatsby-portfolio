77834192b5195fc378c187e59ce3e0bb017373c7
arjun-sudo <arjungautam5431@gmail.com>
2021-04-02 21:09:09 +0545

    download resume

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
 import SEO from "../components/SEO"
+import resume from "../assets/resume/Deedy_CV.pdf"
 
 const About = ({data:{about:{nodes},},}) => {
 
@@ -17,14 +18,29 @@ const About = ({data:{about:{nodes},},}) => {
                  className={"about-img"}/>
 
           <article className={"about-text"}>
+
           <Title title={title}/>
           <p>{info}</p>
+
             <div className="about-stack">
               {stack.map((item)=>{
                 return <span key={item.id}>{item.title}</span>
               })}
+
             </div>
+            <br/>
+            {/*<Link to={""} className={"btn"}>*/}
+
+
+            {/*  Download Resume*/}
+            {/*</Link>*/}
+
+            <button className="btn">
+              <i className="fa fa-download"></i><a href={resume} download={"resume.pdf"}> Download Resume
+            </a></button>
+
           </article>
+
         </div>
       </div>
     </Layout>
